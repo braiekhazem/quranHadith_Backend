628013a46e34320e019c311c2ce451fec0973231
braiekhazem <braiekhazem@takiacademyteam.com>
2023-04-05 11:05:31 +0100

    fix cors error

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ io.on("connection", (socket) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3001", "http://localhost:3000"],
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3000",
+      "https://quranhadith.netlify.app",
+    ],
     credentials: true,
   })
 );
